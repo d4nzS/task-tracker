0a56888bed474d429e12d344db81b1028a403922
d4nzS <dzianis.karym@gmail.com>
2022-07-28 20:41:04 +0300

    add sumbit for the main form

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
 'use strict';
 
+const form = document.forms['form'];
+
+form.onsubmit = function (event) {
+    event.preventDefault();
+
+    const task = {
+        title: form.elements['title'].value,
+        text:  form.elements['text'].value,
+        priority:  form.elements['priority'].value,
+        color:  form.elements['color'].value,
+        date: Date.now()
+    }
+
+    // localStorage.setItem(generateId(), JSON.stringify(task));
+    console.log(task);
+
+    form.reset();
+    $("#exampleModal").modal("hide");
+}
+
+/**
+ * Generate id for task
+ * @returns {string}
+ */
+function generateId() {
+    return "_" + Math.random().toString(36).substr(2, 9);
+}
